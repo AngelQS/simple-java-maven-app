1d9c5004283b0a50ac0b5d440b930cbcaab45511
Angel Quiroz Soriano <angel.quiroz@tismart.com>
2023-02-05 23:44:58 -0500

    Update javamavenDSL3.groovy

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL 3') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/AngelQS/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('AngelQS')
+            node / gitConfigEmail('aedwin.acuario31@gmail.com')
         }
     }
     triggers {
